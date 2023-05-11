3b14a5e36736adb8633748899cf03f28f48325fe
Jason <47520063+JasonLeeForOnly@users.noreply.github.com>
2023-05-11 17:07:14 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Jason Lee's personal AI assistant.
+          Jason Lee&apos;s personal AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
